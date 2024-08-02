f8706d89136e645ca42db43c3c4d49f27942b758
Iván Illera <ivanillera@gmail.com>
2024-08-02 20:31:03 -0300

    update App.css, App.jsx and TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Divider } from 'antd';
+import { AddToDo } from './AddTodo';
 
 export const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { text: 'Aprender React', completed: false},
+    { text: 'Dar de comer al perro', completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { text, completed: false }]);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
-    <div>TodoList</div>
+    <div>
+      <AddToDo/>
+    </div>
   )
 }
